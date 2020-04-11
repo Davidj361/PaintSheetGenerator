49d195ac7f38fcdf6f29622eeef16645eb65d969
pravdeep375 <38989560+pravdeep375@users.noreply.github.com>
2020-04-11 16:23:18 -0400

    fixed the legend size around the img/ added border

--- legend/legend.h
+++ legend/legend.h
@@ -9,7 +9,7 @@
 #include <limits>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
-#include "../kmeans/segmenter.h"
+#include "segmenter.h"
 
 using namespace std;
 using namespace cv;
@@ -19,41 +19,64 @@ class Legend {
 public:
 	Legend(Mat& in, const vector<Segment>& segments) : segments(segments) {
 		cvtColor(in, img, COLOR_GRAY2RGB);
-		for (size_t i=0; i<segments.size(); i++) {
+		for (size_t i = 0; i < segments.size(); i++) {
 			Segment s = segments[i];
 			if (findColour(s.getColour()) == -1) {
 				colours.push_back(s);
 			}
 		}
 	}
-		
+
 	void createLegend(Mat& dst) {
 		// Put numbers on the actual image
 		for (size_t i = 0; i < segments.size(); i++) {
 			int number = findColour(segments[i].getColour());
 			if (number == -1)
 				throw runtime_error("createLegend broke");
-			putText(img, to_string(number + 1), segments[i].getCenter(), FONT_HERSHEY_DUPLEX, 1.0, Scalar::all(255),2);
-			putText(img, to_string(number + 1), segments[i].getCenter(), FONT_HERSHEY_DUPLEX, 1.0, Scalar::all(0),1);
+
+			//number positioning
+			int baseline = 0;
+			Size textSize = getTextSize(to_string(number + 1), Fontface, 1.0, 2, &baseline);
+			
+			// draw the box
+			Rect r = Rect(segments[i].getCenter().x, segments[i].getCenter().y, textSize.width, -textSize.height);
+			rectangle(img, r.tl(), r.br(), Scalar(0, 0, 255));
+
+			putText(img, to_string(number + 1), segments[i].getCenter(), Fontface, 1.0, Scalar::all(255), 2);
+			putText(img, to_string(number + 1), segments[i].getCenter(), Fontface, 1.0, Scalar::all(0), 1);
 		}
 
-		//add padding to bottom and right side of img
-		copyMakeBorder(img, dst, 0, img.rows, 0, img.cols, BORDER_CONSTANT, Scalar::all(255));
+		//add black border around img
+		copyMakeBorder(img, dst, 1, 1, 1, 1, BORDER_CONSTANT, Scalar::all(0));
+
+		//add inital padding to bottom and right side of img
+		copyMakeBorder(dst, dst, 10, 10, 10, 42, BORDER_CONSTANT, Scalar::all(255));
 
 		//point pt1 is rectangle top left corner
 		//point pt2 is rectangle bottom right corner
 		//with 20 pixel spacing between subsuqent rectangles vertically
-		Point pt1 = Point(img.cols + 10, 10); 
+		Point pt1 = Point(img.cols + 20, 10);
 		Point pt2 = Point(pt1.x + 10, pt1.y + 10);
 		Point text;
 
 		// Create the legend
 		for (size_t i = 0; i < colours.size(); i++) {
+
+			//added legend entries to right once they reach the height of the orginal img
+			//and also extend the img by the needed amount 
+			//(rectangle width + text width + extra padding) 10 + 12 + 20
+			if (pt1.y > img.rows) {
+				cout << "max height reached!\ncreating new legend column..." << endl;
+				copyMakeBorder(dst, dst, 0, 0, 0, 30, BORDER_CONSTANT, Scalar::all(255));
+				pt1 = Point(text.x + 20, 10);
+				pt2 = Point(pt1.x + 10, pt1.y + 10);
+			}
+
 			rectangle(dst, pt1, pt2, Scalar(colours[i].getColour()), -1);
 			rectangle(dst, pt1, pt2, Scalar::all(0), 1);
 			text.x = pt1.x + 10;
 			text.y = pt2.y;
-			putText(dst, to_string(i+1), text, FONT_HERSHEY_DUPLEX, 0.5, Scalar::all(0));
+			putText(dst, to_string(i + 1), text, Fontface, 0.5, Scalar::all(0));
 
 			pt1.y += 20;
 			pt2.y += 20;
@@ -64,9 +87,10 @@ private:
 	Mat img;
 	vector<Segment> segments;
 	vector<Segment> colours;
+	int Fontface = FONT_HERSHEY_DUPLEX;
 
 	int findColour(const Scalar& s) const {
-		for (size_t i=0; i<colours.size(); i++) {
+		for (size_t i = 0; i < colours.size(); i++) {
 			Scalar s2 = colours[i].getColour();
 			if (isSameScalar(s, s2)) {
 				return i;
@@ -104,103 +128,8 @@ int edge_only(Mat img, Mat& result) {
 	return 0;
 }
 
-/*
-  Uses kmeans cluster to improve segmentation when finding edges
-  params:
-  img: input image
-  result: output image
-*/
-int myKmeans(Mat img, Mat& result, const int k) {
-	Mat img_data, labels, centers;
-
-	img.convertTo(img_data, CV_32F);
-	img_data = img_data.reshape(1, img.rows * img.cols);
-
-	kmeans(img_data,
-		   k,
-		   labels,
-		   TermCriteria(TermCriteria::Type::MAX_ITER + TermCriteria::Type::EPS, 10, 1.0),
-		   10,
-		   KMEANS_RANDOM_CENTERS,
-		   centers);
-
-	centers = centers.reshape(3);
-	img_data = img_data.reshape(3);
-
-	/*
-	  NOTE: This combines the segments into 1 picture but we could just return a list of Mats each element a colour
-	  then segment
-	*/
-
-	/*
-	  every pixel has a label for what segment it's in
-	  we know the centers (colours) of k segements
-	  let every pixel be the same colour as the center it shares a label with
-	*/
-	Vec3f* pixel = img_data.ptr<Vec3f>();
-	for (int i = 0; i < img_data.rows; i++) {
-		int center_label = labels.at<int>(i);
-		pixel[i] = centers.at<Vec3f>(center_label);
-	}
-
-	Mat result_temp = img_data.reshape(3, img.rows);
-	result_temp.convertTo(result_temp, CV_8U);
-
-	result = result_temp;
-	return 0;
-}
-
-
-// Returns regions that were divided by edges
-void getRegions(Mat& in, vector<Mat>& contours) {
-	vector<Mat> ret;
-	// Go through all white pixels and
-	// for (int col=0; col < in.cols; col++) {
-	// 	for (int row=0; row < in.rows; row++) {
-	// 		//if (in.at<uchar>(row, col) == 0)
-	// 	}
-	// }
-
-	//vector<Mat> contours;
-	vector<Vec4i> hier;
-	threshold(in, in, 125, 255, THRESH_BINARY_INV);
-	// findContours(in, contours, hier, RETR_TREE, CHAIN_APPROX_NONE);
-	findContours(in, contours, RETR_CCOMP, CHAIN_APPROX_TC89_KCOS);
-
-	//display found regions
-	for (uint iter = 0; iter < contours.size(); iter++) {
-		Mat dst = Mat::zeros(in.rows, in.cols, CV_8UC3);
-		drawContours(dst, contours, iter, Scalar(0, 0, 255), FILLED);
-		//display(dst);
-	}
-
-}
-
-//calculates average colour within a contour by creating a mask for each region
-//then adds colours to a list
-void getAverageColour(Mat in, vector<Mat> contours, vector<Scalar>& averageColours) {
-	for (size_t iter = 0; iter < contours.size(); iter++) {
-
-		//create mask for a contour
-		Mat1b mask(in.rows, in.cols, uchar(0));
-		drawContours(mask, contours, iter, Scalar::all(255), FILLED);
-		//display(mask);
-
-		//calculate average colour value for current contour region
-		Scalar average = mean(in, mask);
-
-		//add to list of average colours
-		averageColours.push_back(average);
-
-		Mat dst = Mat(in.rows, in.cols, CV_8UC3, Scalar::all(255));
-		drawContours(dst, contours, iter, average, FILLED);
-		//display(dst);
-	}
-}
-
-
 //Dilation functin to increase thicken the edges
-void Dilation(Mat in, Mat& dilation_dst, int size, int numTimes){
+void Dilation(Mat in, Mat& dilation_dst, int size, int numTimes) {
 	//create kernal for dilation
 	Mat element = getStructuringElement(MORPH_RECT, Size(size, size));
 
@@ -214,7 +143,7 @@ void Dilation(Mat in, Mat& dilation_dst, int size, int numTimes){
 
 		}
 	}
-	
+	bitwise_not(dilation_dst, dilation_dst);
 }
 
 #endif
